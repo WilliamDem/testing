4383f48487c1523a81db2c327fd10b241d690fdf
WilliamDem <95745488+WilliamDem@users.noreply.github.com>
2021-12-08 07:07:07 -0300

    Update datos.php

--- php/datos.php
+++ php/datos.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["mensaje"]) ){
 $to = "grafchac@gmail.com";
-$subject = "Asunto: ".$_POST["asunto"]."\n";
-$contenido .= "Nombre: ".$_POST["nombre"]."\n";
-$contenido .= "Cel: ".$_POST["telefono"]."\n\n";
-$contenido .= "Email: ".$_POST["email"]."\n\n";
-$contenido .= "Comentario: ".$_POST["mensaje"]."\n\n";
-$header = "From: graficachacabuco.com.ar\nReply-To:".$_POST["email"]."\n";
+$subject = "Asunto: ".$_POST["asunto"];
+$contenido .= "Nombre: ".$_POST["nombre"];
+$contenido .= "Cel: ".$_POST["telefono"];
+$contenido .= "Email: ".$_POST["email"];
+$contenido .= "Comentario: ".$_POST["mensaje"];
+$header = "From: graficachacabuco.com.ar\nReply-To:".$_POST["email"];
 $header .= "Mime-Version: 1.0\n";
 $header .= "Content-Type: text/plain";
 if(mail($to, $subject, $contenido ,$header)){
